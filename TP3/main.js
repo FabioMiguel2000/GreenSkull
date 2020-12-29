7f8d4cfd78183d2b50c83aeeb5e705f310606f63
“Fabio <up201806829@fe.up.pt>
2020-12-29 16:35:40 +0000

    sds

--- TP3/main.js
+++ TP3/main.js
@@ -46,7 +46,8 @@ serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.j
     './animation/MyAnimation.js', './animation/MyKeyframeAnimation.js', 'MySpriteSheet.js',
     './primitives/MyPlane.js', './primitives/MyPatch.js', './primitives/MyDefbarrel.js',
     'MySpriteAnimation.js', 'MySpriteText.js', './game/MyGameBoard.js', 'CGFOBJModel.js', 'CGFResourceReader.js',
-    './game/MyPiece.js', './game/MyGreenSkull.js', './game/MyTile.js', './game/MyGameMove.js',
+    './game/MyPiece.js', './game/MyGreenSkull.js', './game/MyTile.js', './game/MyGameMove.js', 'MyGameSequence.js',
+
 
 
     main = function() {
